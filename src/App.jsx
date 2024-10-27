82c35f549dbb6c585417a11a07ff79ff8f666a4b
Deepak Kohli <deepakkohli1810@gmail.com>
2024-10-27 08:02:31 +0530

    minor modification is made in the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Login from './components/Login'
 const App = () => {
   return (
-    <div className='text-white'>
-      This is deepak here
-    </div>
+  < >
+ 
+  <Login/>
+  </>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Login = () => {
+
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    const submitHandler = (e)=>{
+      e.preventDefault()
+     console.log("Hello guysss,,,Form submitted")
+    }
+
+  return (
+    <div className=' w-screen h-screen flex items-center justify-center'>
+      <div className='border-2  px-20 py-24 border-emerald-500 rounded-2xl'>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className='flex flex-col items-center justify-center'>
+        <input 
+        value={email}
+        onChange={(e)=>{
+          // setEmail(e.target.value)
+        }}
+        required
+        className='border-2 text-white outline-none bg-transparent border-emerald-500 rounded-full  py-3 px-5 text-xl'
+         type="email" placeholder='Enter your email' />
+
+        <input
+        required
+         className='border-2 text-white outline-none bg-transparent border-emerald-500 rounded-full  py-3 px-5 text-xl mt-4'
+         type="password" placeholder='Enter password' />
+
+         <button
+         className=' text-white outline-none bg-emerald-500 rounded-full py-3 px-5 text-xl mt-5'
+         > Login </button>
+      </form>
+      </div>
+     
+    </div>
+  )
+}
+
+export default Login
